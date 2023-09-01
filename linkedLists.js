635bf2777e5e24e1d289b27c03bcf27a8f2f0c25
norphel <norphel317@gmail.com>
2023-09-01 20:09:31 +0530

    add size method that returns the total number of nodes in the list

--- linkedLists.js
+++ linkedLists.js
@@ -2,6 +2,7 @@ class LinkedList {
   head() {
     return this.headNode;
   }
+
   prepend(value) {
     const newNode = new Node(value);
 
@@ -12,6 +13,7 @@ class LinkedList {
       this.headNode = newNode;
     }
   }
+
   tail() {
     let tmp = this.head();
     if (!tmp) {
@@ -22,12 +24,25 @@ class LinkedList {
     }
     return tmp;
   }
+
   append(value) {
     if (!this.head()) {
       this.headNode = new Node(value);
     }
     let tailNode = this.tail();
     tailNode.nextNode = new Node(value);
+  }
+  size() {
+    let count = 0;
+    if (!this.head()) {
+      return 0;
+    }
+    let current = this.head();
+    while (current.nextNode !== null) {
+      count++;
+      current = current.nextNode;
+    }
+    return count;
   }
 }
 class Node {
@@ -38,8 +53,7 @@ class Node {
 }
 
 let list = new LinkedList();
-console.log(list);
-list.append(2);
-list.prepend(1);
-list.append(3);
-console.log(list);
+console.log(list.size());
+list.append(1);
+list.append("a");
+console.log(list.size());
